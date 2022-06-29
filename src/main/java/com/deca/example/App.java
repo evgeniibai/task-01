af40c6a7b1b9819fffd7bd4a569c83cabd6a7421
Evgenii Bai <evgenii.bai@decathlon.com>
2022-06-29 14:24:38 +0300

    Writed pom file from scratch: all dependencies include

--- src/main/java/com/deca/example/App.java
+++ src/main/java/com/deca/example/App.java
@@ -1,15 +1,17 @@
 package com.deca.example;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Simple Spring Boot App: Spring Web, Spring Security, Spring JPA, Flyway, H2 Database etc.
  * @author Evgenii Bai
  * @version 1.0
  */
+@SpringBootApplication
 public class App 
 {
-    public static void main( String[] args )
-    {
-
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+        SpringApplication.run(App.class, args);
     }
 }
