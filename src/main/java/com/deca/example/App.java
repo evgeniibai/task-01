c9afe3f15cd05d44a4a36b7c4439609bc634e6ec
Evgenii Bai <evgenii.bai@decathlon.com>
2022-06-29 13:05:54 +0300

    init commit

---
+++ src/main/java/com/deca/example/App.java
@@ -0,0 +1,15 @@
+package com.deca.example;
+
+/**
+ * Simple Spring Boot App: Spring Web, Spring Security, Spring JPA, Flyway, H2 Database etc.
+ * @author Evgenii Bai
+ * @version 1.0
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        System.out.println( "Hello World!" );
+    }
+}
